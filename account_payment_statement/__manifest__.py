25d57bdf29e186ef4fd94d05ee0f5b451921b8cc
agung <agung@portcities.net>
2018-04-13 09:31:28 +0700

    module to choose the bank statement to create statement line of this payment

---
+++ account_payment_statement/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+#    Copyright (C) 2014-2018 Port Cities Indonesia (http://www.portcities.net)
+#    @author Agung Rachmatullah <agung@portcities.net>
+#
+#
+
+{
+    'name': 'Statement Payment',
+    'version': '11.0.1.0.0',
+    'category': 'Accounting',
+    'summary': 'Adding Statement for Payment',
+    'author': 'Agung Rachmatullah',
+    'website': 'https://portcities.net/',
+    'depends': [
+        'account'
+    ],
+    'data': [
+        'views/account.xml'
+    ],
+    'installable': True,
+}
---
+++ account_payment_statement/models/account_invoice.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+""" Payment Statement in Bank """
+from odoo import api, fields, models
+MAP_INVOICE_TYPE_PARTNER_TYPE = {
+    'out_invoice': 'customer',
+    'out_refund': 'customer',
+    'in_invoice': 'supplier',
+    'in_refund': 'supplier',
+}
+
+
+class AccountPaymentInherit(models.Model):
+    _inherit = "account.payment"
+
+    statement_id = fields.Many2one(
+        'account.bank.statement', string='Bank Statement', domain=[('journal_id', '=', 'journal_id.id')]
+    )
+
+    @api.multi
+    def post(self):
+        '''After create account.payment, will create a statement line.
+
+        :return:
+        '''
+        res = super(AccountPaymentInherit, self).post()
+        for rec in self:
+            if rec.statement_id:
+                amount = 0 - rec.amount
+            else:
+                amount = rec.amount
+                vals = {
+                    'statement_id': rec.statement_id.id,
+                    'date': rec.payment_date,
+                    'name': '-',
+                    'partner_id': rec.partner_id.id,
+                    'ref': rec.name,
+                    'amount': amount
+                }
+                self.env['account.bank.statement.line'].create(vals)
+        return res
+
+class AccountRegisterPaymentsInherit(models.TransientModel):
+    _inherit = "account.register.payments"
+
+    statement_id = fields.Many2one(
+        'account.bank.statement', string='Bank Statement', domain=[('journal_id.id', '=', 'journal_id.id')]
+    )
+
+    @api.multi
+    def _prepare_payment_vals(self, invoices):
+        '''Overide to include statement id.
+
+        :param invoices: The invoices that should have the same commercial partner and the same type.
+        :return: The payment values as a dictionary with statement_id included.
+        '''
+        res = super(AccountRegisterPaymentsInherit, self)._prepare_payment_vals(invoices)
+        res['statement_id'] = self.statement_id.id
+        return res
